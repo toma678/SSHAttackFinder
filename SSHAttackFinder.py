831b8d836b48a6a21b11f09d1ac2ff9126279cea
toma678 <toma678@users.noreply.github.com>
2015-04-08 20:52:31 +0100

    Update SSHAttackFinder.py

--- SSHAttackFinder.py
+++ SSHAttackFinder.py
@@ -1,7 +1,26 @@
+"""
+SSHAttackFinder lists IPs of possible brute force attacks via SSH.
+    Copyright (C) 2015  Toma678
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import re
+import os
 
 auth = open("/var/log/auth.log","r").read() #Opens the logfile for reading
-IPsf = open("/var/www/site/IPs", "w+") #Opens the file that holds the IPs for reading and writing
+IPsf = open(os.getcwd()+"/IPs", "w+") #Opens the file that holds the IPs for reading and writing
 IPs = IPsf.read()
 a = re.findall(r'sshd.+Failed.+', auth)
 IPset = set() #Initializes the set
